9fc57ef30164aebfe6b2e2242a6c4a174dba529c
Phong <phongdoan.dev@gmail.com>
2019-12-13 13:25:05 -0800

    movies path works

--- data/schema.sql
+++ data/schema.sql
@@ -3,9 +3,10 @@ DROP TABLE cityLocation, weather, events;
 CREATE TABLE IF NOT EXISTS cityLocation(
     id SERIAL PRIMARY KEY,
     searchQuery VARCHAR(255),
-    formattedQuery VARCHAR(255),
+    formatted_query VARCHAR(255),
     latitude NUMERIC(18,6),
-    longitude NUMERIC(18,6)
+    longitude NUMERIC(18,6),
+    region VARCHAR(255)
 
 );
 
--- server.js
+++ server.js
@@ -10,14 +10,11 @@ const app = express();
 app.use(cors());
 
 // GLOBAL VARIABLES
-let error = {
-  status: 500,
-  responseText: 'Sorry, something went wrong',
-}
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY = process.env.DARKSKY_API_KEY;
 const EVENTBRITE_API_KEY = process.env.EVENTFUL_API_KEY;
 const DATABASE_URL = process.env.DATABASE_URL;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 let query;
 let locationSubmitted;
 
@@ -27,11 +24,12 @@ client.connect();
 
 
 // LOCATION CONSTRUCTOR FUNCTION
-function Geolocation(searchquery, formAddr, lat, lng) {
+function Geolocation(searchquery, formAddr, lat, lng, region) {
   this.searchquery = searchquery;
   this.formatted_query = formAddr;
   this.latitude = lat;
   this.longitude = lng;
+  this.region = region;
 }
 
 // Event CONSTRUCTOR FUNCTION
@@ -48,26 +46,44 @@ function Forecast(summary, time) {
   this.time = (new Date(time * 1000)).toDateString();
 }
 
+//MOVIES CONSTRUCTOR FUNCTION
+function Movies(movie) {
+  this.title = movie.title,
+  this.overview = movie.overview,
+  this.average_votes = movie.vote_average,
+  this.total_votes = movie.vote_count,
+  this.image_url = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+  this.popularity = movie.popularity,
+  this.released_on = movie.released_date
+}
+
 function getWeaterData(request, response) {
-  superagent.get(`https://api.darksky.net/forecast/${WEATHER_API_KEY}/${locationSubmitted.latitude},${locationSubmitted.longitude}`).then(res => {
-    const weatherArr = res.body.daily.data
-    const reply = weatherArr.map(byDay => {
-      return new Forecast(byDay.summary, byDay.time);
+  try {
+    const sql = 'SELECT * FROM cityLocation WHERE searchQuery = $1';
+    client.query(sql, [query]).then(sqlResponse => {
+      superagent.get(`https://api.darksky.net/forecast/${WEATHER_API_KEY}/${sqlResponse.rows[0].latitude},${sqlResponse.rows[0].longitude}`).then(res => {
+        const weatherArr = res.body.daily.data
+        const reply = weatherArr.map(byDay => {
+          return new Forecast(byDay.summary, byDay.time);
+        })
+        response.send(reply);
+      })
     })
-    response.send(reply);
-  })
+  } catch (error) {
+    errorHandler(error, response);
+  }
 }
 
 app.get('/events', (request, response) => {
-  superagent.get(`http://api.eventful.com/json/events/search?where=${locationSubmitted.latitude},${locationSubmitted.longitude}&within=25&app_key=${EVENTBRITE_API_KEY}`).then(res => {
+  superagent.get(`http://api.eventful.com/json/events/search?location=${query}&within=25&app_key=${EVENTBRITE_API_KEY}`).then(res => {
     let events = JSON.parse(res.text);
     let moreEvents = events.events.event
     let eventData = moreEvents.map(event => {
       return new Event(event.url, event.title, event.start_time, event.description)
     })
     response.send(eventData);
-  }).catch(function () {
-    console.log('banana');
+  }).catch(function (error) {
+    console.error(error);
     return null;
   })
 })
@@ -75,30 +91,59 @@ app.get('/events', (request, response) => {
 
 function getGeoData(request, response) {
   query = request.query.data;
-  const sql = 'SELECT * FROM cityLocation WHERE searchQuery = $1';
-  client.query(sql, [query]).then(sqlResponse => {
-    if (sqlResponse.rowCount > 0) {
-      response.send(sqlResponse.rows[0]);
-    } else {
-      createDataFromAPI(request, response, query);
-    }
-  });
+  try {
+    const sql = 'SELECT * FROM cityLocation WHERE searchQuery = $1';
+    client.query(sql, [query]).then(sqlResponse => {
+      if (sqlResponse.rowCount > 0) {
+        response.send(sqlResponse.rows[0]);
+      } else {
+        createDataFromAPI(request, response, query);
+      }
+    })
+  } catch (error) {
+    errorHandler(error, response);
+  }
 }
 
 function createDataFromAPI(request, response, query) {
-  superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${GEOCODE_API_KEY}`).then(geoResponse => {
-    const location = geoResponse.body.results[0].geometry.location;
-    const formAddr = geoResponse.body.results[0].formatted_address;
-    locationSubmitted = new Geolocation(query, formAddr, location.lat, location.lng);
-    const sqlValu = [locationSubmitted.searchquery, locationSubmitted.formatted_query, locationSubmitted.latitude, locationSubmitted.longitude];
-    const SQL = `INSERT INTO cityLocation(
-      searchQuery, formattedQuery, latitude, longitude
+  try {
+    superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${GEOCODE_API_KEY}`).then(geoResponse => {
+      const location = geoResponse.body.results[0].geometry.location;
+      const formAddr = geoResponse.body.results[0].formatted_address;
+      const countryCode = geoResponse.body.results[0].address_components[3].short_name;
+      locationSubmitted = new Geolocation(query, formAddr, location.lat, location.lng, countryCode);
+      const sqlValu = [locationSubmitted.searchquery, locationSubmitted.formatted_query, locationSubmitted.latitude, locationSubmitted.longitude, locationSubmitted.region];
+      const SQL = `INSERT INTO cityLocation(
+      searchQuery, formatted_query, latitude, longitude, region
       ) VALUES (
-        $1, $2, $3, $4
+        $1, $2, $3, $4, $5
         )`;
-    client.query(SQL, sqlValu);
-    response.send(locationSubmitted);
-  })
+      client.query(SQL, sqlValu);
+      response.send(locationSubmitted);
+    })
+  } catch (error) {
+    errorHandler(error, response);
+  }
+}
+
+function getMovieData(movieReq, movieRes) {
+  try {
+    const sql = 'SELECT * FROM cityLocation WHERE searchQuery = $1';
+    client.query(sql, [query]).then(sqlResponse => {
+      superagent.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${MOVIE_API_KEY}&region=${sqlResponse.rows[0].region}`).then(movieDBRes => {
+        const movieList = JSON.parse(movieDBRes.text);
+        const movieArr = movieList.results.map(elem => new Movies(elem));
+        movieRes.send(movieArr);
+      })
+    })
+  } catch (error) {
+    errorHandler(error, movieRes);
+  }
+}
+
+function errorHandler(error, response) {
+  console.error(error);
+  response.status(500).send('Whoops! There is a problem');
 }
 
 // LOCATION PATH
@@ -106,6 +151,9 @@ app.get('/location', getGeoData);
 
 // WEATHER PATH
 app.get('/weather', getWeaterData);
+
+// MOVIEs PAth
+app.get('/movies', getMovieData);
 
 app.listen(PORT, () => {
   console.log(`App is on PORT: ${PORT}`);
